04de8bf2875e1558053f58869752bb62be7f36bb
alexeymu <73878733+alexeymu@users.noreply.github.com>
2021-08-20 00:58:39 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,77 @@
-var name = prompt("Ваше имя?", " ");
-var surname = prompt("Ваша фамилия?", " ");
-var patron = prompt("Ваше отчество?", " ");
-var age = Number(prompt("Ваш возраст (количество лет)?", ""));
-var resulage = age + 5;
-var ageDate = age * 365;
-var isMan = confirm("ваш пол - мужской?");
+window.onload = function() {
+    new_user();
+};
 
-var gender = isMan ? "мужской" : "женский"
-var pensAge = isMan && age >= 60 || !isMan && age >= 55 ? "да" : 'нет';
+function new_user() {
+    var fio = "";
 
-alert('ФИО: ' + name + ' ' + surname + ' ' + patron + '\n\r ' + 'возраст, лет: ' + age +
-    '\n\r ' + 'возраст, дней: ' + ageDate + '\n\r ' + 'Через 5 лет Вам будет: ' + resulage +
-    '\n\r' + " пол: " + gender + '\n\r ' + 'пенсионный возраст: ' + pensAge);
+    var age;
+    var resulAge;
+    var ageDay;
+    var pol;
+    var pens;
+    var pens_confirm;
+
+
+
+    enter_fio();
+
+    enter_age();
+    enter_pol();
+
+
+
+
+    function enter_fio() {
+        function enter_fio(question) {
+            var answer = prompt(question);
+            if (!answer || isFinite(parseInt(answer))) {
+                enter_fio(question);
+            } else {
+                fio += answer + " ";
+            }
+        }
+        enter_fio("Ваша фамилию: ");
+        enter_fio("Ваше имя: ");
+        enter_fio("Ваше отчество: ");
+
+    }
+
+    function enter_age() {
+
+        age = parseInt(prompt("Введите возраст"));
+        if (isFinite(age)) {
+            ageDay = age * 365;
+            resulAge = age + 5;
+        } else {
+            enter_age();
+        }
+    }
+
+    function enter_pol() {
+        pol = confirm("Ваш пол - мужской?");
+        pol ? pens = 65 : pens = 60;
+        if (pens == 65 && age >= pens || pens == 60 && age >= pens) {
+            pens_confirm = "да";
+        } else {
+            pens_confirm = "нет";
+        }
+        if (pol) {
+            pol = "мужской";
+        } else {
+            pol = "женский";
+        }
+    }
+
+
+
+    if (age != 0 && fio.length != 0) {
+        alert('ФИО: ' + fio + '\n\r ' + 'возраст, лет: ' + age +
+            '\n\r ' + 'возраст, дней: ' + ageDay + '\n\r ' + 'Через 5 лет Вам будет: ' + resulAge +
+            '\n\r' + " пол: " + pol + '\n\r ' + 'пенсионный возраст: ' + pens_confirm);
+    } else {
+        alert("Введите данные снова в корректной форме");
+    }
+
+
+}
