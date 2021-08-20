4ce66411d6d9b020a88d36c32ce4442df08aff79
alexeymu <73878733+alexeymu@users.noreply.github.com>
2021-08-20 12:34:01 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -10,14 +10,14 @@ function new_user() {
     var ageDay;
     var pol;
     var pens;
-    var pens_confirm;
+    var pensConfirm;
 
 
 
     enterFio();
 
-    enter_age();
-    enter_pol();
+    enterAge();
+    enterPol();
 
 
 
@@ -37,24 +37,24 @@ function new_user() {
 
     }
 
-    function enter_age() {
+    function enterAge() {
 
         age = parseInt(prompt("Введите возраст"));
         if (isFinite(age)) {
             ageDay = age * 365;
             resulAge = age + 5;
         } else {
-            enter_age();
+            enterAge();
         }
     }
 
-    function enter_pol() {
+    function enterPol() {
         pol = confirm("Ваш пол - мужской?");
         pol ? pens = 65 : pens = 60;
         if (pens === 65 && age >= pens || pens === 60 && age >= pens) {
-            pens_confirm = "да";
+            pensConfirm = "да";
         } else {
-            pens_confirm = "нет";
+            pensConfirm = "нет";
         }
         if (pol) {
             pol = "мужской";
@@ -68,7 +68,7 @@ function new_user() {
     if (age != 0 && fio.length != 0) {
         alert('ФИО: ' + fio + '\n\r ' + 'возраст, лет: ' + age +
             '\n\r ' + 'возраст, дней: ' + ageDay + '\n\r ' + 'Через 5 лет Вам будет: ' + resulAge +
-            '\n\r' + " пол: " + pol + '\n\r ' + 'пенсионный возраст: ' + pens_confirm);
+            '\n\r' + " пол: " + pol + '\n\r ' + 'пенсионный возраст: ' + pensConfirm);
     } else {
         alert("Введите данные снова в корректной форме");
     }
