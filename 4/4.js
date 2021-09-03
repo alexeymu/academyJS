2d337c478690ce57421174e4e2a298dee23dbf16
alexeymu <73878733+alexeymu@users.noreply.github.com>
2021-09-03 20:00:48 +0300

    Update 4.js

--- 4/4.js
+++ 4/4.js
@@ -1,29 +1,24 @@
 /* Создайте функцию, которая принимает число как строку n и возвращает число без конечных и стартовых нулей.
 Конечные нули - это нули после десятичной точки, которые не влияют на значение (например, последние три нуля в 6.7000 и 6.07000).
 Стартовые нули - это нули перед целым числом, которые не влияют на значение (например, первые три нуля в 000427 и 000930).
-
 removeLeadingTrailing("190.000") ➞ "190"
 removeLeadingTrailing("00803") ➞ "803"
 removeLeadingTrailing("07.1200") ➞ "7.12"
 removeLeadingTrailing("90") ➞ "90"
-
 //Вернуть строку.
 //Если вы получаете число с .0 в конце, верните целочисленное значение (например, верните «4», а не «4,0»).
 //Если число 0, 0,0, 000, 00,00 и т. Д. ... верните «0». */
 
 
 /* function removeLeadingTrailing(x) {
-    const parsed = parseInt(x);
+    const parsed = parseFloat(x);
     if (isNaN(parsed)) { return 0; }
     return parsed;
 }
-
 console.log(removeLeadingTrailing("190.000"));
 console.log(removeLeadingTrailing("00803"));
 console.log(removeLeadingTrailing("07.1200"));
 console.log(removeLeadingTrailing("90"));
-
-
  */
 
 
@@ -58,3 +53,4 @@ function treeSum() {
     ];
     console.log('Сумма всех элементов массива: ' + getSum(a));
 }
+treeSum();
